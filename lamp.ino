656b95a490a4b1ba02b4ba3aa3a407cc0cc2408e
Vianney Nguyen <vianney.nguyen@skf.com>
2022-10-23 16:00:51 -0600

    Support led patterns

--- lamp.ino
+++ lamp.ino
@@ -1,5 +1,6 @@
-#define LIGHT_PIN 2
-#define LIGHT_COUNT 4
+#define LED_PIN 2
+#define LED_PATTERN_SIZE 2
+#define LED_COUNT 4
 // 0.125 us +- 0.15
 // 0.25
 // 0.375
@@ -15,41 +16,53 @@
 // T1L 0.45us
 
 struct Lights {
-    uint8_t green;
-    uint8_t red;
-    uint8_t blue;
+    uint8_t count = 1;
+    
+    uint8_t colors[3 * LED_PATTERN_SIZE];
+
+    void setColor(uint8_t index, uint8_t red, uint8_t green, uint8_t blue) {
+        const uint8_t offset = 3 * index;
+        colors[offset + 0] = green;
+        colors[offset + 1] = red;
+        colors[offset + 2] = blue;
+    }
 } lights;
 
 void setupLights(struct Lights &lights) {
-    lights.green = 0xFF;
-    lights.red = 0x00;
-    lights.blue = 0x0F;
+    lights.setColor(0, 0x00, 0xFF, 0x00);
+    lights.setColor(1, 0xFF, 0x00, 0x00);
 
-    pinMode(LIGHT_PIN, OUTPUT);
+    pinMode(LED_PIN, OUTPUT);
 }
 
 void writeLights(struct Lights &lights) {
-    const uint8_t mask = digitalPinToBitMask(LIGHT_PIN);
+    const uint8_t mask = digitalPinToBitMask(LED_PIN);
     volatile uint8_t hi = PORTB | mask;
     volatile uint8_t lo = PORTB & (~mask);
-    const uint32_t data = ((uint32_t) lights.blue << 16) | ((uint32_t) lights.red << 8) | ((uint32_t) lights.green); // AVR is little-endian
-    volatile uint8_t *start = ((uint8_t*) &data);
-    volatile uint8_t byte = *start;
-    volatile uint8_t temp = (data & 0x80) ? hi : lo;
-    volatile uint8_t light = 0b0111;
-    volatile uint8_t count = LIGHT_COUNT;
+    volatile uint8_t *start = ((uint8_t*) lights.colors);
+    volatile uint8_t data = *start;
+    volatile uint8_t next = (data & 0x80) ? hi : lo;
+    volatile uint8_t colorIndex;
+    volatile uint8_t colorCount = 0x00;
+    volatile uint8_t ledCount = LED_COUNT;
     volatile uint8_t zero = 0;
 
+    for (int i = 0; i < LED_PATTERN_SIZE * 3; i++) {
+        colorCount <<= 1;
+        colorCount |= 0x01;
+    }
+    colorIndex = colorCount;
+    
 
     asm volatile(
         "top: \n\t"
         
         "out %[port], %[hi] \n\t"   // 1    port = HI
         "rjmp .+0 \n\t"             //      nop nop
-        "out %[port], %[temp] \n\t" // 1    port = TEMP
-        "mov %[temp], %[lo] \n\t"   // 1    TEMP = LO
-        "sbrc %[byte], 6 \n\t"      // 1/2  if byte & 0x40
-        "mov %[temp], %[hi] \n\t"   // 1    TEMP = HI
+        "out %[port], %[next] \n\t" // 1    port = next
+        "mov %[next], %[lo] \n\t"   // 1    next = LO
+        "sbrc %[data], 6 \n\t"      // 1/2  if data & 0x40
+        "mov %[next], %[hi] \n\t"   // 1    next = HI
         "nop \n\t"
         "out %[port], %[lo] \n\t"
         "rjmp .+0 \n\t"
@@ -57,10 +70,10 @@ void writeLights(struct Lights &lights) {
 
         "out %[port], %[hi] \n\t"   // 1    port = HI
         "rjmp .+0 \n\t"             //      nop nop
-        "out %[port], %[temp] \n\t" // 1    port = TEMP
-        "mov %[temp], %[lo] \n\t"   // 1    TEMP = LO
-        "sbrc %[byte], 5 \n\t"      // 1/2  if byte & 0x40
-        "mov %[temp], %[hi] \n\t"   // 1    TEMP = HI
+        "out %[port], %[next] \n\t" // 1    port = next
+        "mov %[next], %[lo] \n\t"   // 1    next = LO
+        "sbrc %[data], 5 \n\t"      // 1/2  if data & 0x40
+        "mov %[next], %[hi] \n\t"   // 1    next = HI
         "nop \n\t"
         "out %[port], %[lo] \n\t"
         "rjmp .+0 \n\t"
@@ -68,10 +81,10 @@ void writeLights(struct Lights &lights) {
 
         "out %[port], %[hi] \n\t"   // 1    port = HI
         "rjmp .+0 \n\t"             //      nop nop
-        "out %[port], %[temp] \n\t" // 1    port = TEMP
-        "mov %[temp], %[lo] \n\t"   // 1    TEMP = LO
-        "sbrc %[byte], 4 \n\t"      // 1/2  if byte & 0x40
-        "mov %[temp], %[hi] \n\t"   // 1    TEMP = HI
+        "out %[port], %[next] \n\t" // 1    port = next
+        "mov %[next], %[lo] \n\t"   // 1    next = LO
+        "sbrc %[data], 4 \n\t"      // 1/2  if data & 0x40
+        "mov %[next], %[hi] \n\t"   // 1    next = HI
         "nop \n\t"
         "out %[port], %[lo] \n\t"
         "rjmp .+0 \n\t"
@@ -79,10 +92,10 @@ void writeLights(struct Lights &lights) {
 
         "out %[port], %[hi] \n\t"   // 1    port = HI
         "rjmp .+0 \n\t"             //      nop nop
-        "out %[port], %[temp] \n\t" // 1    port = TEMP
-        "mov %[temp], %[lo] \n\t"   // 1    TEMP = LO
-        "sbrc %[byte], 3 \n\t"      // 1/2  if byte & 0x40
-        "mov %[temp], %[hi] \n\t"   // 1    TEMP = HI
+        "out %[port], %[next] \n\t" // 1    port = next
+        "mov %[next], %[lo] \n\t"   // 1    next = LO
+        "sbrc %[data], 3 \n\t"      // 1/2  if data & 0x40
+        "mov %[next], %[hi] \n\t"   // 1    next = HI
         "nop \n\t"
         "out %[port], %[lo] \n\t"
         "rjmp .+0 \n\t"
@@ -90,10 +103,10 @@ void writeLights(struct Lights &lights) {
 
         "out %[port], %[hi] \n\t"   // 1    port = HI
         "rjmp .+0 \n\t"             //      nop nop
-        "out %[port], %[temp] \n\t" // 1    port = TEMP
-        "mov %[temp], %[lo] \n\t"   // 1    TEMP = LO
-        "sbrc %[byte], 2 \n\t"      // 1/2  if byte & 0x40
-        "mov %[temp], %[hi] \n\t"   // 1    TEMP = HI
+        "out %[port], %[next] \n\t" // 1    port = next
+        "mov %[next], %[lo] \n\t"   // 1    next = LO
+        "sbrc %[data], 2 \n\t"      // 1/2  if data & 0x40
+        "mov %[next], %[hi] \n\t"   // 1    next = HI
         "nop \n\t"
         "out %[port], %[lo] \n\t"
         "rjmp .+0 \n\t"
@@ -101,51 +114,52 @@ void writeLights(struct Lights &lights) {
 
         "out %[port], %[hi] \n\t"   // 1    port = HI
         "rjmp .+0 \n\t"             // 2    nop nop
-        "out %[port], %[temp] \n\t" // 1    port = TEMP
-        "mov %[temp], %[lo] \n\t"   // 1    TEMP = LO
-        "sbrc %[byte], 1 \n\t"      // 1/2  if byte & 0x40
-        "mov %[temp], %[hi] \n\t"   // 1      TEMP = HI
+        "out %[port], %[next] \n\t" // 1    port = next
+        "mov %[next], %[lo] \n\t"   // 1    next = LO
+        "sbrc %[data], 1 \n\t"      // 1/2  if data & 0x40
+        "mov %[next], %[hi] \n\t"   // 1      next = HI
         "nop \n\t"                  // 1    nop
         "out %[port], %[lo] \n\t"   // 1    port = LO
         "rjmp .+0 \n\t"             // 2    nop nop
-        "lsr %[light] \n\t"         // 1    light >> 1
+        "lsr %[ci] \n\t"            // 1    light >> 1
 
         "out %[port], %[hi] \n\t"   // 1    port = HI
-        "sbrs %[light], 0 \n\t"     // 1/2  if light == 0
-        "subi %[count], 1 \n\t"     // 1      count--
-        "out %[port], %[temp] \n\t" // 1    port = TEMP
-        "sbrs %[light], 0 \n\t"     // 1/2  if light == 0 (not literally, but equivalent for our scenario)
+        "sbrs %[ci], 0 \n\t"        // 1/2  if light == 0
+        "subi %[lc], 1 \n\t"        // 1      count--
+        "out %[port], %[next] \n\t" // 1    port = next
+        "sbrs %[ci], 0 \n\t"        // 1/2  if light == 0 (not literally, but equivalent for our scenario)
         "mov %[ptr], %[start] \n\t" // 1      ptr = start
-        "sbrs %[light], 0 \n\t"     // 1/2  if light == 0
-        "ldi %[light], 7 \n\t"      // 1      light = 7 (0b111)
+        "sbrs %[ci], 0 \n\t"        // 1/2  if light == 0
+        "mov %[ci], %[cc] \n\t"     // 1      light = colorCount
         "out %[port], %[lo] \n\t"   // 1    port = LO
-        "mov %[temp], %[lo] \n\t"   // 1    temp = LO
-        "sbrc %[byte], 0 \n\t"      // 1/2  if byte & 0x01
-        "mov %[temp], %[hi] \n\t"   // 1      temp = HI
+        "mov %[next], %[lo] \n\t"   // 1    next = LO
+        "sbrc %[data], 0 \n\t"      // 1/2  if data & 0x01
+        "mov %[next], %[hi] \n\t"   // 1      next = HI
 
         "out %[port], %[hi] \n\t"   // 1    port = HI
-        "ld %[byte], %a[ptr]+ \n\t" // 2    byte = *ptr++
-        "out %[port], %[temp] \n\t" // 1    port = TEMP
+        "ld %[data], %a[ptr]+ \n\t" // 2    data = *ptr++
+        "out %[port], %[next] \n\t" // 1    port = next
         "nop \n\t"                  // 1    nop
-        "mov %[temp], %[lo] \n\t"   // 1    temp = LO
-        "sbrc %[byte], 7 \n\t"      // 1/2  if byte & 0x80
-        "mov %[temp], %[hi] \n\t"   // 1      temp = HI
+        "mov %[next], %[lo] \n\t"   // 1    next = LO
+        "sbrc %[data], 7 \n\t"      // 1/2  if data & 0x80
+        "mov %[next], %[hi] \n\t"   // 1      next = HI
         "out %[port], %[lo] \n\t"   // 1    port = LO
-        "cpse %[count], %[zero] \n\t"     // 1/2  if count != 0
+        "cpse %[lc], %[zero] \n\t"  // 1/2  if count != 0
         "rjmp top \n\t"             // 2    goto top
 
-        : [temp] "+r" (temp),
-          [light] "+r" (light),
-          [count] "+r" (count)
+        : [next] "+r" (next),
+          [ci] "+r" (colorIndex),
+          [lc] "+r" (ledCount)
         : [port] "I" (_SFR_IO_ADDR(PORTB)),
           [hi] "r" (hi),
           [lo] "r" (lo),
-          [byte] "r" (byte),
+          [data] "r" (data),
+          [cc] "r" (colorCount),
           [zero] "r" (zero),
           [start] "e" (start),
           [ptr] "e" (start+1)
     );
-    digitalWrite(LIGHT_PIN, LOW);
+    digitalWrite(LED_PIN, LOW);
 }
 
 void setup() {
